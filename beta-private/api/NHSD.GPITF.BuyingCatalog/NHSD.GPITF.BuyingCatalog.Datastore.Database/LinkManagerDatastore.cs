c0be0e996c366f127876de39b4545748b9a74800
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-08-21 15:14:08 +0100

    - only need to create a link between Solution<->Framework
    - all other links are created in DB/CRM

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.Database/LinkManagerDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.Database/LinkManagerDatastore.cs
@@ -13,34 +13,6 @@ namespace NHSD.GPITF.BuyingCatalog.Datastore.Database
     {
     }
 
-    public void CapabilityFrameworkCreate(string frameworkId, string capabilityId)
-    {
-      GetInternal(() =>
-      {
-        using (var trans = _dbConnection.Value.BeginTransaction())
-        {
-          var entity = new CapabilityFramework { FrameworkId = frameworkId, CapabilityId = capabilityId };
-          _dbConnection.Value.Insert(entity, trans);
-          trans.Commit();
-          return 0;
-        }
-      });
-    }
-
-    public void CapabilityStandardCreate(string capabilityId, string standardId, bool isOptional)
-    {
-      GetInternal(() =>
-      {
-        using (var trans = _dbConnection.Value.BeginTransaction())
-        {
-          var entity = new CapabilityStandard { CapabilityId = capabilityId, StandardId = standardId, IsOptional= isOptional };
-          _dbConnection.Value.Insert(entity, trans);
-          trans.Commit();
-          return 0;
-        }
-      });
-    }
-
     public void FrameworkSolutionCreate(string frameworkId, string solutionId)
     {
       GetInternal(() =>
@@ -48,20 +20,6 @@ namespace NHSD.GPITF.BuyingCatalog.Datastore.Database
         using (var trans = _dbConnection.Value.BeginTransaction())
         {
           var entity = new FrameworkSolution { FrameworkId = frameworkId, SolutionId = solutionId };
-          _dbConnection.Value.Insert(entity, trans);
-          trans.Commit();
-          return 0;
-        }
-      });
-    }
-
-    public void FrameworkStandardCreate(string frameworkId, string standardId)
-    {
-      GetInternal(() =>
-      {
-        using (var trans = _dbConnection.Value.BeginTransaction())
-        {
-          var entity = new FrameworkStandard { FrameworkId = frameworkId, StandardId = standardId };
           _dbConnection.Value.Insert(entity, trans);
           trans.Commit();
           return 0;
