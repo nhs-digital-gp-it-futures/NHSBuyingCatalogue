9ae8a59548af350b68234a2fe559a9a95bf083ff
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-08-24 11:00:09 +0100

    - refactored into ClaimsLogicBase

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/CapabilitiesImplementedLogic.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/CapabilitiesImplementedLogic.cs
@@ -1,40 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Models;
-using System.Linq;
 
 namespace NHSD.GPITF.BuyingCatalog.Logic
 {
-  public sealed class CapabilitiesImplementedLogic : LogicBase, ICapabilitiesImplementedLogic
+  public sealed class CapabilitiesImplementedLogic : ClaimsLogicBase<CapabilitiesImplemented>, ICapabilitiesImplementedLogic
   {
-    private readonly ICapabilitiesImplementedDatastore _datastore;
-
     public CapabilitiesImplementedLogic(
-      ICapabilitiesImplementedDatastore datastore, 
+      ICapabilitiesImplementedDatastore datastore,
       IHttpContextAccessor context) :
-      base(context)
+      base(datastore, context)
     {
-      _datastore = datastore;
-    }
-
-    public IQueryable<CapabilitiesImplemented> BySolution(string solutionId)
-    {
-      return _datastore.BySolution(solutionId);
-    }
-
-    public CapabilitiesImplemented Create(CapabilitiesImplemented claimedcapability)
-    {
-      return _datastore.Create(claimedcapability);
-    }
-
-    public void Delete(CapabilitiesImplemented claimedcapability)
-    {
-      _datastore.Delete(claimedcapability);
-    }
-
-    public void Update(CapabilitiesImplemented claimedcapability)
-    {
-      _datastore.Update(claimedcapability);
     }
   }
 }
--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/StandardsApplicableLogic.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/StandardsApplicableLogic.cs
@@ -1,40 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Models;
-using System.Linq;
 
 namespace NHSD.GPITF.BuyingCatalog.Logic
 {
-  public sealed class StandardsApplicableLogic : LogicBase, IStandardsApplicableLogic
+  public sealed class StandardsApplicableLogic : ClaimsLogicBase<StandardsApplicable>, IStandardsApplicableLogic
   {
-    private readonly IStandardsApplicableDatastore _datastore;
-
     public StandardsApplicableLogic(
       IStandardsApplicableDatastore datastore,
       IHttpContextAccessor context) :
-      base(context)
+      base(datastore, context)
     {
-      _datastore = datastore;
-    }
-
-    public IQueryable<StandardsApplicable> BySolution(string solutionId)
-    {
-      return _datastore.BySolution(solutionId);
-    }
-
-    public StandardsApplicable Create(StandardsApplicable claimedstandard)
-    {
-      return _datastore.Create(claimedstandard);
-    }
-
-    public void Update(StandardsApplicable claimedstandard)
-    {
-      _datastore.Update(claimedstandard);
-    }
-
-    public void Delete(StandardsApplicable claimedstandard)
-    {
-      _datastore.Delete(claimedstandard);
     }
   }
 }
