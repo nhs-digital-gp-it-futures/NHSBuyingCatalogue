2e22d1c80d602707c7e117a3e6fbca5e8355cc7e
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-08-24 10:37:58 +0100

    - removed filters+validators as not doing anything

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/CapabilitiesImplementedLogic.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/CapabilitiesImplementedLogic.cs
@@ -1,5 +1,4 @@
-﻿using FluentValidation;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Models;
 using System.Linq;
@@ -9,43 +8,32 @@ namespace NHSD.GPITF.BuyingCatalog.Logic
   public sealed class CapabilitiesImplementedLogic : LogicBase, ICapabilitiesImplementedLogic
   {
     private readonly ICapabilitiesImplementedDatastore _datastore;
-    private readonly ICapabilitiesImplementedValidator _validator;
-    private readonly ICapabilitiesImplementedFilter _filter;
 
     public CapabilitiesImplementedLogic(
       ICapabilitiesImplementedDatastore datastore, 
-      IHttpContextAccessor context,
-      ICapabilitiesImplementedValidator validator,
-      ICapabilitiesImplementedFilter filter) :
+      IHttpContextAccessor context) :
       base(context)
     {
       _datastore = datastore;
-      _validator = validator;
-      _filter = filter;
     }
 
     public IQueryable<CapabilitiesImplemented> BySolution(string solutionId)
     {
-      var claimedCaps = _datastore.BySolution(solutionId);
-      claimedCaps.ToList().ForEach(cc => _validator.ValidateAndThrow(cc));
-      return _filter.Filter(claimedCaps);
+      return _datastore.BySolution(solutionId);
     }
 
     public CapabilitiesImplemented Create(CapabilitiesImplemented claimedcapability)
     {
-      _validator.ValidateAndThrow(claimedcapability);
-      return _filter.Filter(new[] { _datastore.Create(claimedcapability) }.AsQueryable()).SingleOrDefault();
+      return _datastore.Create(claimedcapability);
     }
 
     public void Delete(CapabilitiesImplemented claimedcapability)
     {
-      _validator.ValidateAndThrow(claimedcapability);
       _datastore.Delete(claimedcapability);
     }
 
     public void Update(CapabilitiesImplemented claimedcapability)
     {
-      _validator.ValidateAndThrow(claimedcapability);
       _datastore.Update(claimedcapability);
     }
   }
--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/StandardsApplicableLogic.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/StandardsApplicableLogic.cs
@@ -1,5 +1,4 @@
-﻿using FluentValidation;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Models;
 using System.Linq;
@@ -9,43 +8,32 @@ namespace NHSD.GPITF.BuyingCatalog.Logic
   public sealed class StandardsApplicableLogic : LogicBase, IStandardsApplicableLogic
   {
     private readonly IStandardsApplicableDatastore _datastore;
-    private readonly IStandardsApplicableValidator _validator;
-    private readonly IStandardsApplicableFilter _filter;
 
     public StandardsApplicableLogic(
       IStandardsApplicableDatastore datastore,
-      IHttpContextAccessor context,
-      IStandardsApplicableValidator validator,
-      IStandardsApplicableFilter filter) :
+      IHttpContextAccessor context) :
       base(context)
     {
       _datastore = datastore;
-      _validator = validator;
-      _filter = filter;
     }
 
     public IQueryable<StandardsApplicable> BySolution(string solutionId)
     {
-      var claimedStds = _datastore.BySolution(solutionId);
-      claimedStds.ToList().ForEach(cs => _validator.ValidateAndThrow(cs));
-      return _filter.Filter(claimedStds);
+      return _datastore.BySolution(solutionId);
     }
 
     public StandardsApplicable Create(StandardsApplicable claimedstandard)
     {
-      _validator.ValidateAndThrow(claimedstandard);
-      return _filter.Filter(new[] { _datastore.Create(claimedstandard) }.AsQueryable()).SingleOrDefault();
+      return _datastore.Create(claimedstandard);
     }
 
     public void Update(StandardsApplicable claimedstandard)
     {
-      _validator.ValidateAndThrow(claimedstandard);
       _datastore.Update(claimedstandard);
     }
 
     public void Delete(StandardsApplicable claimedstandard)
     {
-      _validator.ValidateAndThrow(claimedstandard);
       _datastore.Delete(claimedstandard);
     }
   }
