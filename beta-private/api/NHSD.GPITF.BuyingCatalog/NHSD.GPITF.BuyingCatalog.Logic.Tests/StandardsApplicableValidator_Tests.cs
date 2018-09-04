e5e5c20a32ffe964e57e1b7635e7cab305eb8f24
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-09-04 12:09:46 +0100

    - initial test

---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic.Tests/StandardsApplicableValidator_Tests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NHSD.GPITF.BuyingCatalog.Interfaces;
+using NHSD.GPITF.BuyingCatalog.Models;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace NHSD.GPITF.BuyingCatalog.Logic.Tests
+{
+  [TestFixture]
+  public sealed class StandardsApplicableValidator_Tests
+  {
+    private Mock<IHttpContextAccessor> _context;
+    private Mock<IStandardsApplicableDatastore> _claimDatastore;
+    private Mock<ISolutionsDatastore> _solutionsDatastore;
+
+    [SetUp]
+    public void SetUp()
+    {
+      _context = new Mock<IHttpContextAccessor>();
+      _claimDatastore = new Mock<IStandardsApplicableDatastore>();
+      _solutionsDatastore = new Mock<ISolutionsDatastore>();
+    }
+
+    [Test]
+    public void Constructor_Completes()
+    {
+      Assert.DoesNotThrow(() => new StandardsApplicableValidator(_context.Object, _claimDatastore.Object, _solutionsDatastore.Object));
+    }
+
+  }
+}
