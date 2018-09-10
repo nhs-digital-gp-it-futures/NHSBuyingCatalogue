ae9a22d0d7049b4706f043e35bc58180192ec651
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-09-10 11:57:45 +0100

    - restrict/bound type

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog/Interfaces/IClaimsDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog/Interfaces/IClaimsDatastore.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using NHSD.GPITF.BuyingCatalog.Models;
+using System.Collections.Generic;
 
 namespace NHSD.GPITF.BuyingCatalog.Interfaces
 {
 #pragma warning disable CS1591
-  public interface IClaimsDatastore<T>
+  public interface IClaimsDatastore<T> where T : ClaimsBase
   {
     T ById(string id);
     IEnumerable<T> BySolution(string solutionId);
