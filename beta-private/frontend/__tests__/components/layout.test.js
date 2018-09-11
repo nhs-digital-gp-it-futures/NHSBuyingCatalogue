9906306c9f0289f6a116482f777cc8908f929920
Paul Mitchell (BJSS) <paul.mitchell@bjss.com>
2018-09-11 16:43:37 +0100

    [#199] Setup Jest gubbins and a single meaningful test of the Layout component

---
+++ beta-private/frontend/__tests__/components/layout.test.js
@@ -0,0 +1,14 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+
+import Layout from '../../components/layout.js'
+
+describe('Layout component', () => {
+  it('emits <title> tag beginning with supplied title prop', () => {
+    const app = shallow(<Layout title="Something!" />)
+
+    expect(app.find('title').text()).toEqual(
+      expect.stringMatching(/^Something!/)
+    )
+  })
+})
