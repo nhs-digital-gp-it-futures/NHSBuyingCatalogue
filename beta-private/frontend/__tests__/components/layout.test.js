f331db434f6edfbe8d8c8fbb15ad202599e96164
Paul Mitchell (BJSS) <paul.mitchell@bjss.com>
2018-09-12 09:26:26 +0100

    [#199] Prepare for CI build with unit test reporting on Azure

--- beta-private/frontend/__tests__/components/layout.test.js
+++ beta-private/frontend/__tests__/components/layout.test.js
@@ -5,10 +5,12 @@ import Layout from '../../components/layout.js'
 
 describe('Layout component', () => {
   it('emits <title> tag beginning with supplied title prop', () => {
-    const app = shallow(<Layout title="Something!" />)
+    const app = shallow(<Layout title='Something!' />)
 
     expect(app.find('title').text()).toEqual(
       expect.stringMatching(/^Something!/)
     )
   })
 })
+
+/* eslint-env jest */
