6124f822e47956dea64ed97e836a9bfb7f23d32c
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-09-17 14:12:48 +0100

    - remove requirement for https since API back end will be inside a container

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog/WebHostBuilder.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog/WebHostBuilder.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Net;
 
 namespace NHSD.GPITF.BuyingCatalog
 {
@@ -18,46 +15,9 @@ namespace NHSD.GPITF.BuyingCatalog
         .Build();
 
       return WebHost.CreateDefaultBuilder(args)
-        .UseStartup<Startup>()
-        .UseKestrel(options =>
-        {
-          var httpsInfos = new Dictionary<int, IPAddress>();
-          var allUrls = (config.GetValue<string>("urls") ?? "http://localhost:5000").Split(';');
-          foreach (var url in allUrls)
-          {
-            var updatedUrl = url;
-            if (updatedUrl.Contains("*"))
-            {
-              updatedUrl = updatedUrl.Replace("*", "0.0.0.0");
-            }
-
-            var listenUrl = new Uri(updatedUrl);
-            if (listenUrl.Scheme == "https")
-            {
-              httpsInfos.Add(listenUrl.Port, IPAddress.Parse(listenUrl.Host));
-              continue;
-            }
-            options.Listen(listenUrl.IsLoopback ? IPAddress.Loopback : IPAddress.Any, listenUrl.Port);
-          }
-
-          var certCfg = config.GetSection("Certificate");
-          var certFileName = certCfg.GetValue<string>("FileName") ?? "NHSD.GPITF.BuyingCatalog.pfx";
-          if (!string.IsNullOrEmpty(certFileName))
-          {
-            var httpsPort = certCfg.GetValue<int>(Constants.HttpsPortKey) == 0 ? 8000 : certCfg.GetValue<int>(Constants.HttpsPortKey);
-            httpsInfos.Add(httpsPort, IPAddress.Any);
-
-            foreach (var kvp in httpsInfos)
-            {
-              options.Listen(kvp.Value, kvp.Key, listenOptions =>
-              {
-                var certPassword = certCfg.GetValue<string>("Password") ?? "DisruptTheMarket";
-                listenOptions.UseHttps(certFileName, certPassword);
-              });
-            }
-          }
-        })
         .UseConfiguration(config)
+        .UseKestrel()
+        .UseStartup<Startup>()
         .Build();
     }
   }
