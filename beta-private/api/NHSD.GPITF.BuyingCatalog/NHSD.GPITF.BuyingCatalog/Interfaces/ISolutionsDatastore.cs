f3ebf063363a15b663ab1815c1cda274a06fa781
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-09-26 10:12:33 +0100

    - added EvidenceBlobStore backed onto SharePoint

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog/Interfaces/ISolutionsDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog/Interfaces/ISolutionsDatastore.cs
@@ -1,5 +1,6 @@
 ﻿using NHSD.GPITF.BuyingCatalog.Models;
 using System.Collections.Generic;
+using System.IO;
 
 namespace NHSD.GPITF.BuyingCatalog.Interfaces
 {
@@ -13,16 +14,31 @@ namespace NHSD.GPITF.BuyingCatalog.Interfaces
     void Update(Solutions solution);
   }
 
-  public interface IEvidenceBlobDatastore
+  public interface IEvidenceBlobStore
   {
     void PrepareForSolution(string solutionId);
+
+    /// <summary>
+    /// Upload a file to support a claim
+    /// </summary>
+    /// <param name="claimId">unique identifier of solution claim</param>
+    /// <param name="file">Stream representing file to be uploaded</param>
+    /// <param name="filename">name of file on the server</param>
+    /// <param name="subFolder">optional sub-folder under claim</param>
+    /// <returns>externally accessible URL of file</returns>
+    string AddEvidenceForClaim(string claimId, Stream file, string filename, string subFolder = null);
+
+//API to list folders(paged)
+//API to list files(paged)
+//API to download file
+//API to upload file
   }
 
-  public interface ICapabilitiesImplementedEvidenceBlobDatastore : IEvidenceBlobDatastore
+  public interface ICapabilitiesImplementedEvidenceBlobStore : IEvidenceBlobStore
   {
   }
 
-  public interface IStandardsApplicbleEvidenceBlobDatastore : IEvidenceBlobDatastore
+  public interface IStandardsApplicbleEvidenceBlobStore : IEvidenceBlobStore
   {
   }
 #pragma warning restore CS1591
