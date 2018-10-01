004db279524a5232e8420c9a119959eaafbff4e6
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-10-01 13:38:54 +0100

    - unit tests for business rules

---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.EvidenceBlobStore.SharePoint.Tests/ClaimsEvidenceBlobStoreValidatorBase_Tests.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NHSD.GPITF.BuyingCatalog.Interfaces;
+using NHSD.GPITF.BuyingCatalog.Models;
+using NUnit.Framework;
+using System;
+
+namespace NHSD.GPITF.BuyingCatalog.EvidenceBlobStore.SharePoint.Tests
+{
+  [TestFixture]
+  public sealed class ClaimsEvidenceBlobStoreValidatorBase_Tests
+  {
+    private Mock<IHttpContextAccessor> _context;
+    private Mock<ISolutionsDatastore> _solutionsDatastore;
+    private Mock<IClaimsDatastore<ClaimsBase>> _claimsDatastore;
+
+    [SetUp]
+    public void SetUp()
+    {
+      _context = new Mock<IHttpContextAccessor>();
+      _solutionsDatastore = new Mock<ISolutionsDatastore>();
+      _claimsDatastore = new Mock<IClaimsDatastore<ClaimsBase>>();
+    }
+
+    [Test]
+    public void Constructor_Completes()
+    {
+      Assert.DoesNotThrow(() => new DummyClaimsEvidenceBlobStoreValidatorBase(_context.Object, _solutionsDatastore.Object, _claimsDatastore.Object));
+    }
+
+    [Test]
+    public void MustBeValidClaim_Valid_Succeeds()
+    {
+      var claimId = Guid.NewGuid().ToString();
+      _claimsDatastore.Setup(x => x.ById(claimId)).Returns(Creator.GetClaimsBase());
+      var validator = new DummyClaimsEvidenceBlobStoreValidatorBase(_context.Object, _solutionsDatastore.Object, _claimsDatastore.Object);
+
+      validator.MustBeValidClaim();
+      var valres = validator.Validate(claimId);
+
+      valres.Errors.Should().BeEmpty();
+    }
+
+    [Test]
+    public void MustBeValidClaim_Invalid_ReturnsError()
+    {
+      var claimId = Guid.NewGuid().ToString();
+      var validator = new DummyClaimsEvidenceBlobStoreValidatorBase(_context.Object, _solutionsDatastore.Object, _claimsDatastore.Object);
+
+      validator.MustBeValidClaim();
+      var valres = validator.Validate(claimId);
+
+      valres.Errors.Should()
+        .ContainSingle(x => x.ErrorMessage == "Could not find claim")
+        .And
+        .HaveCount(1);
+    }
+
+    [Test]
+    public void MustBeSameOrganisation_Same_Succeeds()
+    {
+      var orgId = Guid.NewGuid().ToString();
+      var ctx = Creator.GetContext(orgId: orgId);
+      _context.Setup(c => c.HttpContext).Returns(ctx);
+      var claimId = Guid.NewGuid().ToString();
+      var claim = Creator.GetClaimsBase();
+      _claimsDatastore.Setup(x => x.ById(claimId)).Returns(claim);
+      var soln = Creator.GetSolution(orgId: orgId);
+      _solutionsDatastore.Setup(x => x.ById(claim.SolutionId)).Returns(soln);
+      var validator = new DummyClaimsEvidenceBlobStoreValidatorBase(_context.Object, _solutionsDatastore.Object, _claimsDatastore.Object);
+
+      validator.MustBeSameOrganisation();
+      var valres = validator.Validate(claimId);
+
+      valres.Errors.Should().BeEmpty();
+    }
+
+    [Test]
+    public void MustBeSameOrganisation_Different_ReturnsError()
+    {
+      var orgId = Guid.NewGuid().ToString();
+      var ctx = Creator.GetContext();
+      _context.Setup(c => c.HttpContext).Returns(ctx);
+      var claimId = Guid.NewGuid().ToString();
+      var claim = Creator.GetClaimsBase();
+      _claimsDatastore.Setup(x => x.ById(claimId)).Returns(claim);
+      var soln = Creator.GetSolution(orgId: orgId);
+      _solutionsDatastore.Setup(x => x.ById(claim.SolutionId)).Returns(soln);
+      var validator = new DummyClaimsEvidenceBlobStoreValidatorBase(_context.Object, _solutionsDatastore.Object, _claimsDatastore.Object);
+
+      validator.MustBeSameOrganisation();
+      var valres = validator.Validate(claimId);
+
+      valres.Errors.Should()
+        .ContainSingle(x => x.ErrorMessage == "Cannot add/see evidence for other organisation")
+        .And
+        .HaveCount(1);
+    }
+  }
+}
---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.EvidenceBlobStore.SharePoint.Tests/Creator.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using NHSD.GPITF.BuyingCatalog.Models;
+using System;
+using System.Security.Claims;
+
+namespace NHSD.GPITF.BuyingCatalog.EvidenceBlobStore.SharePoint.Tests
+{
+  internal static class Creator
+  {
+    public static DefaultHttpContext GetContext(
+      string orgId = "NHS Digital",
+      string role = Roles.Admin,
+      string email = "NHS-GPIT@WigglyAmps.com")
+    {
+      var orgClaim = new Claim(nameof(Organisations), orgId);
+      var roleClaim = new Claim(ClaimTypes.Role, role);
+      var emailClaim = new Claim(ClaimTypes.Email, email);
+      var claimsIdentity = new ClaimsIdentity(new[] { orgClaim, roleClaim, emailClaim });
+      var user = new ClaimsPrincipal(new[] { claimsIdentity });
+      var ctx = new DefaultHttpContext { User = user };
+
+      return ctx;
+    }
+
+    public static Organisations GetOrganisation(
+      string id = "NHS Digital",
+      string primaryRoleId = PrimaryRole.GovernmentDepartment)
+    {
+      return new Organisations
+      {
+        Id = id,
+        Name = id,
+        PrimaryRoleId = primaryRoleId
+      };
+    }
+
+    public static Solutions GetSolution(
+      string id = null,
+      string previousId = null,
+      string orgId = null,
+      SolutionStatus status = SolutionStatus.Draft)
+    {
+      return new Solutions
+      {
+        Id = id ?? Guid.NewGuid().ToString(),
+        PreviousId = previousId,
+        OrganisationId = orgId ?? Guid.NewGuid().ToString(),
+        Status = status
+      };
+    }
+
+    public static Contacts GetContact(
+      string id = null,
+      string orgId = null)
+    {
+      return new Contacts
+      {
+        Id = id ?? Guid.NewGuid().ToString(),
+        OrganisationId = orgId ?? Guid.NewGuid().ToString()
+      };
+    }
+
+    public static ClaimsBase GetClaimsBase(
+      string id = null,
+      string solnId = null)
+    {
+      return new DummyClaimsBase
+      {
+        Id = id ?? Guid.NewGuid().ToString(),
+        SolutionId = solnId ?? Guid.NewGuid().ToString()
+      };
+    }
+  }
+}
