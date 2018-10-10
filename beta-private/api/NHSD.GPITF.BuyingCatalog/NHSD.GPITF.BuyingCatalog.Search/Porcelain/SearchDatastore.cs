22b3cc9188c9f87ea4058ef32ce536272099e872
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-10-10 14:35:09 +0100

    - added retry via Polly
    - log search keyword

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Search/Porcelain/SearchDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Search/Porcelain/SearchDatastore.cs
@@ -2,6 +2,7 @@
 using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Interfaces.Porcelain;
 using NHSD.GPITF.BuyingCatalog.Models.Porcelain;
+using Polly;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,6 +10,8 @@ namespace NHSD.GPITF.BuyingCatalog.Search.Porcelain
 {
   public sealed class SearchDatastore : ISearchDatastore
   {
+    private readonly ILogger<SearchDatastore> _logger;
+    private readonly ISyncPolicy _policy;
     private readonly IFrameworksDatastore _frameworkDatastore;
     private readonly ISolutionsDatastore _solutionDatastore;
     private readonly ICapabilitiesDatastore _capabilityDatastore;
@@ -24,6 +27,8 @@ namespace NHSD.GPITF.BuyingCatalog.Search.Porcelain
       ICapabilitiesImplementedDatastore claimedCapabilityDatastore,
       ISolutionsExDatastore solutionExDatastore)
     {
+      _logger = logger;
+      _policy = policy.Build(_logger);
       _frameworkDatastore = frameworkDatastore;
       _solutionDatastore = solutionDatastore;
       _capabilityDatastore = capabilityDatastore;
@@ -33,41 +38,47 @@ namespace NHSD.GPITF.BuyingCatalog.Search.Porcelain
 
     public IEnumerable<SearchResult> ByKeyword(string keyword)
     {
-      // get all Frameworks
-      var allFrameworks = _frameworkDatastore.GetAll();
+      // TODO   point logger to database
+      _logger.LogInformation($"{nameof(SearchDatastore)}.{nameof(ByKeyword)}: {keyword}");
 
-      // get all Solutions via frameworks
-      var allSolns = allFrameworks
-        .SelectMany(fw => _solutionDatastore.ByFramework(fw.Id));
+      return _policy.Execute(() =>
+      {
+        // get all Frameworks
+        var allFrameworks = _frameworkDatastore.GetAll();
 
-      // get all Capabilities via frameworks
-      var allCapsFrameworks = allFrameworks
-        .SelectMany(fw => _capabilityDatastore.ByFramework(fw.Id));
+        // get all Solutions via frameworks
+        var allSolns = allFrameworks
+          .SelectMany(fw => _solutionDatastore.ByFramework(fw.Id));
 
-      // get all Capabilities with keyword
-      var allCapsKeywordIds = allCapsFrameworks
-        .Where(cap =>
-          cap.Name.ToLowerInvariant().Contains(keyword.ToLowerInvariant()) ||
-          cap.Description.ToLowerInvariant().Contains(keyword.ToLowerInvariant()))
-        .Select(cap => cap.Id);
+        // get all Capabilities via frameworks
+        var allCapsFrameworks = allFrameworks
+          .SelectMany(fw => _capabilityDatastore.ByFramework(fw.Id));
 
-      // get all unique Solutions with at least one ClaimedCapability with keyword
-      var allSolnsCapsKeyword = allSolns
-        .Where(soln => _claimedCapabilityDatastore
-          .BySolution(soln.Id)
-          .Select(cc => cc.CapabilityId)
-          .Intersect(allCapsKeywordIds)
-          .Any())
-        .Distinct();
+        // get all Capabilities with keyword
+        var allCapsKeywordIds = allCapsFrameworks
+          .Where(cap =>
+            cap.Name.ToLowerInvariant().Contains(keyword.ToLowerInvariant()) ||
+            cap.Description.ToLowerInvariant().Contains(keyword.ToLowerInvariant()))
+          .Select(cap => cap.Id);
 
-      var searchResults = allSolnsCapsKeyword.Select(soln =>
-        new SearchResult
-        {
-          SolutionEx = _solutionExDatastore.BySolution(soln.Id),
-          Distance = _claimedCapabilityDatastore.BySolution(soln.Id).Count() - allCapsKeywordIds.Count()
-        });
+        // get all unique Solutions with at least one ClaimedCapability with keyword
+        var allSolnsCapsKeyword = allSolns
+          .Where(soln => _claimedCapabilityDatastore
+            .BySolution(soln.Id)
+            .Select(cc => cc.CapabilityId)
+            .Intersect(allCapsKeywordIds)
+            .Any())
+          .Distinct();
 
-      return searchResults;
+        var searchResults = allSolnsCapsKeyword.Select(soln =>
+          new SearchResult
+          {
+            SolutionEx = _solutionExDatastore.BySolution(soln.Id),
+            Distance = _claimedCapabilityDatastore.BySolution(soln.Id).Count() - allCapsKeywordIds.Count()
+          });
+
+        return searchResults;
+      });
     }
   }
 }
