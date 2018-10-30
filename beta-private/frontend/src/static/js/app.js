383a1747d4120da6296123852bc936b79526c5ea
Paul Mitchell (BJSS) <paul.mitchell@bjss.com>
2018-10-30 12:53:50 +0000

    [#407] Parameterise header height (and other things), add script to compoensate for anchored links scrolling behing the header

--- beta-private/frontend/src/static/js/app.js
+++ beta-private/frontend/src/static/js/app.js
@@ -1,4 +1,4 @@
-/* global Document, Element */
+/* global $, Document, Element */
 
 // jQuery-esque shorthand for common element selection operations
 window.$ = function $ (selector, el) {
@@ -16,3 +16,13 @@ Document.prototype.$ = Element.prototype.$ = function (selector) {
 Document.prototype.$$ = Element.prototype.$$ = function (selector) {
   return Array.from(this.querySelectorAll(selector))
 }
+
+// Compensate for fixed page header height causing anchored links to appear
+// off-screen on page load. Note that this can't be DOMContentLoaded as the
+// initial scroll position isn't set at that point.
+window.addEventListener('load', () => {
+  const adjustmentElement = $('body > header')
+  const adjustment = adjustmentElement.offsetHeight
+  const scrollingElement = document.scrollingElement || document.documentElement
+  scrollingElement.scrollTop = Math.max(0, scrollingElement.scrollTop - adjustment)
+})
