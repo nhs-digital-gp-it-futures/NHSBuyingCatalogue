1aaaf616f7d0626a95b3acf03eaa40e105d283f2
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-11-01 15:40:45 +0000

    - change to CRM implementations of CapabilitiesImplementedDatastore

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/CapabilitiesImplementedDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/CapabilitiesImplementedDatastore.cs
@@ -2,7 +2,6 @@
 using NHSD.GPITF.BuyingCatalog.Datastore.CRM.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Models;
-using System;
 using System.Collections.Generic;
 
 namespace NHSD.GPITF.BuyingCatalog.Datastore.CRM
@@ -17,29 +16,61 @@ namespace NHSD.GPITF.BuyingCatalog.Datastore.CRM
     {
     }
 
+    private string ResourceBase { get; } = "/CapabilitiesImplemented";
+
     public CapabilitiesImplemented ById(string id)
     {
-      throw new NotImplementedException();
+      return GetInternal(() =>
+      {
+        var request = GetRequest($"{ResourceBase}/ById/{id}");
+        var retval = GetResponse<CapabilitiesImplemented>(request);
+
+        return retval;
+      });
     }
 
     public IEnumerable<CapabilitiesImplemented> BySolution(string solutionId)
     {
-      throw new NotImplementedException();
+      return GetInternal(() =>
+      {
+        var request = GetAllRequest($"{ResourceBase}/BySolution/{solutionId}");
+        var retval = GetResponse<PaginatedList<CapabilitiesImplemented>>(request);
+
+        return retval.Items;
+      });
     }
 
     public CapabilitiesImplemented Create(CapabilitiesImplemented claimedcapability)
     {
-      throw new NotImplementedException();
+      return GetInternal(() =>
+      {
+        var request = GetPutRequest($"{ResourceBase}", claimedcapability);
+        var retval = GetResponse<CapabilitiesImplemented>(request);
+
+        return retval;
+      });
     }
 
     public void Delete(CapabilitiesImplemented claimedcapability)
     {
-      throw new NotImplementedException();
+      GetInternal(() =>
+      {
+        var request = GetDeleteRequest($"{ResourceBase}", claimedcapability);
+        var resp = GetRawResponse(request);
+
+        return 0;
+      });
     }
 
     public void Update(CapabilitiesImplemented claimedcapability)
     {
-      throw new NotImplementedException();
+      GetInternal(() =>
+      {
+        var request = GetPostRequest($"{ResourceBase}", claimedcapability);
+        var resp = GetRawResponse(request);
+
+        return 0;
+      });
     }
   }
 }
