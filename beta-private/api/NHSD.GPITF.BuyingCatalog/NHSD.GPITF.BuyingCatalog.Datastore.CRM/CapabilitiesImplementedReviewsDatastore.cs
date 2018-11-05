8018b7d020853badaa0f9687ff01f6072c75bd35
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-11-05 10:21:58 +0000

    - refactored into ReviewsDatastoreBase

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/CapabilitiesImplementedReviewsDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/CapabilitiesImplementedReviewsDatastore.cs
@@ -2,65 +2,19 @@
 using NHSD.GPITF.BuyingCatalog.Datastore.CRM.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Models;
-using System.Collections.Generic;
 
 namespace NHSD.GPITF.BuyingCatalog.Datastore.CRM
 {
-  public sealed class CapabilitiesImplementedReviewsDatastore : DatastoreBase<CapabilitiesImplementedReviews>, ICapabilitiesImplementedReviewsDatastore
+  public sealed class CapabilitiesImplementedReviewsDatastore : ReviewsDatastoreBase<CapabilitiesImplementedReviews>, ICapabilitiesImplementedReviewsDatastore
   {
+    protected override string ResourceBase { get; } = "/CapabilitiesImplementedReviews";
+
     public CapabilitiesImplementedReviewsDatastore(
       IRestClientFactory crmFactory,
       ILogger<DatastoreBase<CapabilitiesImplementedReviews>> logger,
       ISyncPolicyFactory policy) :
       base(crmFactory, logger, policy)
     {
-    }
-
-    private string ResourceBase { get; } = "/CapabilitiesImplementedReviews";
-
-    public IEnumerable<IEnumerable<CapabilitiesImplementedReviews>> ByEvidence(string evidenceId)
-    {
-      return GetInternal(() =>
-      {
-        var request = GetRequest($"{ResourceBase}/ByEvidence/{evidenceId}");
-        var retval = GetResponse<IEnumerable<IEnumerable<CapabilitiesImplementedReviews>>>(request);
-
-        return retval;
-      });
-    }
-
-    public CapabilitiesImplementedReviews ById(string id)
-    {
-      return GetInternal(() =>
-      {
-        var request = GetRequest($"{ResourceBase}/ById/{id}");
-        var retval = GetResponse<CapabilitiesImplementedReviews>(request);
-
-        return retval;
-      });
-    }
-
-    public CapabilitiesImplementedReviews Create(CapabilitiesImplementedReviews review)
-    {
-      return GetInternal(() =>
-      {
-        review.Id = UpdateId(review.Id);
-        var request = GetPostRequest($"{ResourceBase}", review);
-        var retval = GetResponse<CapabilitiesImplementedReviews>(request);
-
-        return retval;
-      });
-    }
-
-    public void Delete(CapabilitiesImplementedReviews review)
-    {
-      GetInternal(() =>
-      {
-        var request = GetDeleteRequest($"{ResourceBase}", review);
-        var resp = GetRawResponse(request);
-
-        return 0;
-      });
     }
   }
 }
