5cce104fc0f151b3cb5ee144a3f9fe7a68ca5826
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-11-05 08:55:53 +0000

    - meta implementation of SolutionsEx.BySolution

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/Porcelain/SolutionsExDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/Porcelain/SolutionsExDatastore.cs
@@ -4,22 +4,85 @@ using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Interfaces.Porcelain;
 using NHSD.GPITF.BuyingCatalog.Models.Porcelain;
 using System;
+using System.Linq;
 
 namespace NHSD.GPITF.BuyingCatalog.Datastore.CRM.Porcelain
 {
   public sealed class SolutionsExDatastore : DatastoreBase<SolutionEx>, ISolutionsExDatastore
   {
+    private readonly ISolutionsDatastore _solutionDatastore;
+    private readonly ITechnicalContactsDatastore _technicalContactDatastore;
+
+    private readonly ICapabilitiesImplementedDatastore _claimedCapabilityDatastore;
+    private readonly ICapabilitiesImplementedEvidenceDatastore _claimedCapabilityEvidenceDatastore;
+    private readonly ICapabilitiesImplementedReviewsDatastore _claimedCapabilityReviewsDatastore;
+
+    private readonly IStandardsApplicableDatastore _claimedStandardDatastore;
+    private readonly IStandardsApplicableEvidenceDatastore _claimedStandardEvidenceDatastore;
+    private readonly IStandardsApplicableReviewsDatastore _claimedStandardReviewsDatastore;
+
     public SolutionsExDatastore(
       IRestClientFactory crmConnectionFactory, 
       ILogger<SolutionsExDatastore> logger, 
-      ISyncPolicyFactory policy) :
+      ISyncPolicyFactory policy,
+
+      ISolutionsDatastore solutionDatastore,
+      ITechnicalContactsDatastore technicalContactDatastore,
+
+      ICapabilitiesImplementedDatastore claimedCapabilityDatastore,
+      ICapabilitiesImplementedEvidenceDatastore claimedCapabilityEvidenceDatastore,
+      ICapabilitiesImplementedReviewsDatastore claimedCapabilityReviewsDatastore,
+
+      IStandardsApplicableDatastore claimedStandardDatastore,
+      IStandardsApplicableEvidenceDatastore claimedStandardEvidenceDatastore,
+      IStandardsApplicableReviewsDatastore claimedStandardReviewsDatastore
+      ) :
       base(crmConnectionFactory, logger, policy)
     {
+      _solutionDatastore = solutionDatastore;
+      _technicalContactDatastore = technicalContactDatastore;
+
+      _claimedCapabilityDatastore = claimedCapabilityDatastore;
+      _claimedCapabilityEvidenceDatastore = claimedCapabilityEvidenceDatastore;
+      _claimedCapabilityReviewsDatastore = claimedCapabilityReviewsDatastore;
+
+      _claimedStandardDatastore = claimedStandardDatastore;
+      _claimedStandardEvidenceDatastore = claimedStandardEvidenceDatastore;
+      _claimedStandardReviewsDatastore = claimedStandardReviewsDatastore;
     }
 
     public SolutionEx BySolution(string solutionId)
     {
-      throw new NotImplementedException();
+      return GetInternal(() =>
+      {
+        var retval = new SolutionEx
+        {
+          Solution = _solutionDatastore.ById(solutionId),
+          TechnicalContact = _technicalContactDatastore.BySolution(solutionId).ToList(),
+          ClaimedCapability = _claimedCapabilityDatastore.BySolution(solutionId).ToList(),
+          ClaimedStandard = _claimedStandardDatastore.BySolution(solutionId).ToList()
+        };
+
+        // populate Evidence + Review
+        retval.ClaimedCapabilityEvidence = retval.ClaimedCapability
+          .SelectMany(cc => _claimedCapabilityEvidenceDatastore.ByClaim(cc.Id))
+            .SelectMany(x => x)
+            .ToList();
+        retval.ClaimedCapabilityReview = retval.ClaimedCapabilityEvidence
+          .SelectMany(cce => _claimedCapabilityReviewsDatastore.ByEvidence(cce.Id))
+            .SelectMany(x => x)
+            .ToList();
+        retval.ClaimedStandardEvidence = retval.ClaimedStandard
+          .SelectMany(cs => _claimedStandardEvidenceDatastore.ByClaim(cs.Id))
+            .SelectMany(x => x)
+            .ToList();
+        retval.ClaimedStandardReview = retval.ClaimedStandardEvidence
+          .SelectMany(cse => _claimedStandardReviewsDatastore.ByEvidence(cse.Id))
+            .SelectMany(x => x)
+            .ToList();
+
+        return retval;
+      });
     }
 
     public void Update(SolutionEx solnEx)
