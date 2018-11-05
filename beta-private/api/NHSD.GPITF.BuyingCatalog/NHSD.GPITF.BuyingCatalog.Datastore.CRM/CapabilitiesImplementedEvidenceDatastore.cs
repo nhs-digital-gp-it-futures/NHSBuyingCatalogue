48b8b0b25fb71d520ad9bc5521247fed6f6c2a4d
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-11-05 09:43:41 +0000

    - refactored into EvidenceDatastoreBase

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/CapabilitiesImplementedEvidenceDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/CapabilitiesImplementedEvidenceDatastore.cs
@@ -2,54 +2,19 @@
 using NHSD.GPITF.BuyingCatalog.Datastore.CRM.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Models;
-using System.Collections.Generic;
 
 namespace NHSD.GPITF.BuyingCatalog.Datastore.CRM
 {
-  public sealed class CapabilitiesImplementedEvidenceDatastore : DatastoreBase<CapabilitiesImplementedEvidence>, ICapabilitiesImplementedEvidenceDatastore
+  public sealed class CapabilitiesImplementedEvidenceDatastore : EvidenceDatastoreBase<CapabilitiesImplementedEvidence>, ICapabilitiesImplementedEvidenceDatastore
   {
+    protected override string ResourceBase { get; } = "/CapabilitiesImplementedEvidence";
+
     public CapabilitiesImplementedEvidenceDatastore(
       IRestClientFactory crmFactory,
       ILogger<DatastoreBase<CapabilitiesImplementedEvidence>> logger,
       ISyncPolicyFactory policy) :
       base(crmFactory, logger, policy)
     {
-    }
-
-    private string ResourceBase { get; } = "/CapabilitiesImplementedEvidence";
-
-    public IEnumerable<IEnumerable<CapabilitiesImplementedEvidence>> ByClaim(string claimId)
-    {
-      return GetInternal(() =>
-      {
-        var request = GetRequest($"{ResourceBase}/ByClaim/{claimId}");
-        var retval = GetResponse<IEnumerable<IEnumerable<CapabilitiesImplementedEvidence>>>(request);
-
-        return retval;
-      });
-    }
-
-    public CapabilitiesImplementedEvidence ById(string id)
-    {
-      return GetInternal(() =>
-      {
-        var request = GetRequest($"{ResourceBase}/ById/{id}");
-        var retval = GetResponse<CapabilitiesImplementedEvidence>(request);
-
-        return retval;
-      });
-    }
-
-    public CapabilitiesImplementedEvidence Create(CapabilitiesImplementedEvidence evidence)
-    {
-      return GetInternal(() =>
-      {
-        evidence.Id = UpdateId(evidence.Id);
-        var request = GetPostRequest($"{ResourceBase}", evidence);
-        var retval = GetResponse<CapabilitiesImplementedEvidence>(request);
-
-        return retval;
-      });
     }
   }
 }
--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/StandardsApplicableEvidenceDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/StandardsApplicableEvidenceDatastore.cs
@@ -2,53 +2,18 @@
 using NHSD.GPITF.BuyingCatalog.Datastore.CRM.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Models;
-using System.Collections.Generic;
 
 namespace NHSD.GPITF.BuyingCatalog.Datastore.CRM
 {
-  public sealed class StandardsApplicableEvidenceDatastore : DatastoreBase<StandardsApplicableEvidence>, IStandardsApplicableEvidenceDatastore
+  public sealed class StandardsApplicableEvidenceDatastore : EvidenceDatastoreBase<StandardsApplicableEvidence>, IStandardsApplicableEvidenceDatastore
   {
+    protected override string ResourceBase { get; } = "/StandardsApplicableEvidence";
+
     public StandardsApplicableEvidenceDatastore(
       IRestClientFactory crmFactory,
       ILogger<DatastoreBase<StandardsApplicableEvidence>> logger,
       ISyncPolicyFactory policy) : base(crmFactory, logger, policy)
     {
-    }
-
-    private string ResourceBase { get; } = "/StandardsApplicableEvidence";
-
-    public IEnumerable<IEnumerable<StandardsApplicableEvidence>> ByClaim(string claimId)
-    {
-      return GetInternal(() =>
-      {
-        var request = GetRequest($"{ResourceBase}/ByClaim/{claimId}");
-        var retval = GetResponse<IEnumerable<IEnumerable<StandardsApplicableEvidence>>>(request);
-
-        return retval;
-      });
-    }
-
-    public StandardsApplicableEvidence ById(string id)
-    {
-      return GetInternal(() =>
-      {
-        var request = GetRequest($"{ResourceBase}/ById/{id}");
-        var retval = GetResponse<StandardsApplicableEvidence>(request);
-
-        return retval;
-      });
-    }
-
-    public StandardsApplicableEvidence Create(StandardsApplicableEvidence evidence)
-    {
-      return GetInternal(() =>
-      {
-        evidence.Id = UpdateId(evidence.Id);
-        var request = GetPostRequest($"{ResourceBase}", evidence);
-        var retval = GetResponse<StandardsApplicableEvidence>(request);
-
-        return retval;
-      });
     }
   }
 }
