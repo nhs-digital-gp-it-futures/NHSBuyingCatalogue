a9d48fd898e9b7f990699ff21d73740a5884d418
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-11-08 15:23:15 +0000

    - implement ICapabilityStandardDatastore in CRM

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/CapabilityStandardDatastore.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/CapabilityStandardDatastore.cs
@@ -1,15 +1,33 @@
-﻿using NHSD.GPITF.BuyingCatalog.Interfaces;
+﻿using Microsoft.Extensions.Logging;
+using NHSD.GPITF.BuyingCatalog.Datastore.CRM.Interfaces;
+using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Models;
-using System;
 using System.Collections.Generic;
 
 namespace NHSD.GPITF.BuyingCatalog.Datastore.CRM
 {
-  public sealed class CapabilityStandardDatastore : ICapabilityStandardDatastore
+  public sealed class CapabilityStandardDatastore : DatastoreBase<CapabilityStandard>, ICapabilityStandardDatastore
   {
+    public CapabilityStandardDatastore(
+      IRestClientFactory crmFactory,
+      ILogger<DatastoreBase<CapabilityStandard>> logger,
+      ISyncPolicyFactory policy) :
+      base(crmFactory, logger, policy)
+    {
+    }
+
+    private string ResourceBase { get; } = "/CapabilityStandard";
+
     public IEnumerable<CapabilityStandard> GetAll()
     {
-      throw new NotImplementedException();
+      return GetInternal(() =>
+      {
+        //throw new NotImplementedException();
+        var request = GetAllRequest($"{ResourceBase}");
+        var retval = GetResponse<PaginatedList<CapabilityStandard>>(request);
+
+        return retval.Items;
+      });
     }
   }
 }
