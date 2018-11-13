9802502c191b2a359555eff14616f4c874f11d48
Paul Mitchell (BJSS) <paul.mitchell@bjss.com>
2018-11-13 10:56:09 +0000

    🔩 #523 Introduce data provider method for checking solution uniqueness

--- beta-private/frontend/lib/catalogue-data/data-provider.test.js
+++ beta-private/frontend/lib/catalogue-data/data-provider.test.js
@@ -299,6 +299,73 @@ describe('solutionsForSupplierDashboard', () => {
   })
 })
 
+describe('validateSolutionUniqueness', () => {
+  beforeEach(() => {
+    subject.solutionsApi.apiSolutionsByOrganisationByOrganisationIdGet.mockReturnValue({
+      items: [
+        { id: 'id1', name: 'This is a duplicate', version: '1' },
+        { id: 'id2', name: 'This is not a duplicate', version: '2' }
+      ]
+    })
+  })
+
+  it('correctly detects duplicate name and version for new solutions', async () => {
+    let result = await subject.validateSolutionUniqueness({
+      name: 'This is a duplicate',
+      version: '1',
+      organisationId: 'orgId'
+    })
+    expect(result).toBeFalsy()
+
+    result = await subject.validateSolutionUniqueness({
+      name: 'This is not a duplicate',
+      version: '1',
+      organisationId: 'orgId'
+    })
+    expect(result).toBeTruthy()
+
+    result = await subject.validateSolutionUniqueness({
+      name: 'This is not a duplicate',
+      version: '2',
+      organisationId: 'orgId'
+    })
+    expect(result).toBeFalsy()
+
+    result = await subject.validateSolutionUniqueness({
+      name: 'This is a duplicate',
+      version: '2',
+      organisationId: 'orgId'
+    })
+    expect(result).toBeTruthy()
+  })
+
+  it('correctly detects duplicate name and version for existing solutions', async () => {
+    let result = await subject.validateSolutionUniqueness({
+      id: 'id1',
+      name: 'This is a duplicate',
+      version: '1',
+      organisationId: 'orgId'
+    })
+    expect(result).toBeTruthy()
+
+    result = await subject.validateSolutionUniqueness({
+      id: 'id1',
+      name: 'This is not a duplicate',
+      version: '1',
+      organisationId: 'orgId'
+    })
+    expect(result).toBeTruthy()
+
+    result = await subject.validateSolutionUniqueness({
+      id: 'id1',
+      name: 'This is not a duplicate',
+      version: '2',
+      organisationId: 'orgId'
+    })
+    expect(result).toBeFalsy()
+  })
+})
+
 describe('capabilityMappings', () => {
   it('correctly maps the incoming static data', async () => {
     subject.capabilityMappingsApi.apiPorcelainCapabilityMappingsGet.mockReturnValue(
