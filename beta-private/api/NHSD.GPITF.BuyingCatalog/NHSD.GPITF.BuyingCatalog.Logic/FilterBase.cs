6495f1f799775a3101ec556af91180cdaa916334
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-11-14 13:02:28 +0000

    - refactored

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/FilterBase.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/FilterBase.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Reflection;
 
 namespace NHSD.GPITF.BuyingCatalog.Logic
 {
@@ -21,7 +17,7 @@ namespace NHSD.GPITF.BuyingCatalog.Logic
 
     private T FilterInternal(T input)
     {
-      Verify(input);
+      Verifier.Verify(input);
 
       return Filter(input);
     }
@@ -29,38 +25,6 @@ namespace NHSD.GPITF.BuyingCatalog.Logic
     public IEnumerable<T> Filter(IEnumerable<T> input)
     {
       return input.Select(x => FilterInternal(x)).Where(x => x != null);
-    }
-
-    private static void Verify(object obj)
-    {
-      if (obj == null)
-      {
-        return;
-      }
-
-      var props = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-      foreach (var prop in props)
-      {
-        if (prop.PropertyType.GetInterfaces().Contains(typeof(IEnumerable)) &&
-          prop.PropertyType != typeof(string))
-        {
-          foreach (var item in (IEnumerable)prop.GetValue(obj, null))
-          {
-            Verify(item);
-          }
-        }
-
-        var required = prop.GetCustomAttribute<RequiredAttribute>();
-        if (required != null && !required.AllowEmptyStrings)
-        {
-          var value = prop.GetValue(obj) as string;
-          if (value != null &&
-            string.IsNullOrWhiteSpace(value))
-          {
-            throw new InvalidOperationException($"Value is null or whitespace:  {prop.ReflectedType}.{prop.Name}");
-          }
-        }
-      }
     }
   }
 }
---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/Verifier.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace NHSD.GPITF.BuyingCatalog.Logic
+{
+  public static class Verifier
+  {
+    public static void Verify(object obj)
+    {
+      if (obj == null)
+      {
+        return;
+      }
+
+      var props = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+      foreach (var prop in props)
+      {
+        if (prop.PropertyType.GetInterfaces().Contains(typeof(IEnumerable)) &&
+          prop.PropertyType != typeof(string))
+        {
+          foreach (var item in (IEnumerable)prop.GetValue(obj, null))
+          {
+            Verify(item);
+          }
+        }
+
+        var required = prop.GetCustomAttribute<RequiredAttribute>();
+        if (required != null && !required.AllowEmptyStrings)
+        {
+          var value = prop.GetValue(obj) as string;
+          if (value != null &&
+            string.IsNullOrWhiteSpace(value))
+          {
+            throw new InvalidOperationException($"Value is null or whitespace:  {prop.ReflectedType}.{prop.Name}");
+          }
+        }
+      }
+    }
+  }
+}
