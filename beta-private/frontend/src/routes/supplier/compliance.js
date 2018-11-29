f01b7b6082c8324cfafa1f766304abea3ef63486
Adam Slack <adam.slack@bjss.com>
2018-11-29 09:57:31 +0000

    🔩 #404 Enough of the compliance evidence page to integrate Sharepoint

--- beta-private/frontend/src/routes/supplier/compliance.js
+++ beta-private/frontend/src/routes/supplier/compliance.js
@@ -1,6 +1,16 @@
 const _ = require('lodash')
 const router = require('express').Router({ strict: true, mergeParams: true })
+const path = require('path')
+
 const { dataProvider } = require('catalogue-data')
+const { sharePointProvider } = require('catalogue-sharepoint')
+
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage }).any()
+
+// Multer is required to be used before CSRF for multi-part form parsing
+router.use(upload)
 
 // all routes in this module require CSRF protection
 router.use(require('csurf')())
@@ -22,6 +32,14 @@ router
 router
   .route('/:solution_id/evidence/:claim_id/')
   .get(solutionComplianceEvidencePageGet)
+
+router
+  .route('/:solution_id/evidence/:claim_id/')
+  .post(solutionComplianceEvidencePagePost)
+
+router
+  .route('/:solution_id/evidence/:claim_id/:file_name')
+  .get(downloadEvidenceGet)
 
 function commonComplianceContext (req) {
   return {
@@ -59,21 +77,99 @@ async function solutionComplianceDashboard (req, res) {
 async function evidencePageContext (req) {
   const context = {
     ...commonComplianceContext(req),
-    ...await dataProvider.capabilityMappings()
+    ...await dataProvider.capabilityMappings(),
   }
 
   context.claim = _.find(context.solution.standards, { id: req.params.claim_id })
   context.claim.standard = context.standards[context.claim.standardId]
+
+  context.errors = req.body.errors || []
+
+  try {
+    context.files = await fetchFiles(context.claim.id)
+  } catch (err) {
+    context.errors.push(err)
+  }
 
   return context
 }
 
 async function solutionComplianceEvidencePageGet (req, res) {
   const context = {
-    ...await evidencePageContext(req)
+    ...await evidencePageContext(req),
+    errors: req.body.errors || []
+  }
+
+  let latestFile
+
+  if(context.files) {
+    latestFile = findLatestFile(context.files.items)
+  }
+
+  if(latestFile) {
+    latestFile.downloadURL = path.join(req.baseUrl, req.path, latestFile.name)
+
+    context.latestFile = latestFile  
   }
 
   res.render('supplier/compliance/evidence', context)
+}
+
+async function solutionComplianceEvidencePagePost (req, res) {
+  const context = {
+    ...await evidencePageContext(req)
+  }
+
+  if (!req.files.length) {
+    req.body.errors = 'No file to upload.'
+  }
+  const fileToUpload = req.files[0]
+
+  try{
+    await uploadFile(context.claim.id, fileToUpload.buffer, fileToUpload.originalname)
+  }
+  catch (err) {
+    req.body.errors = [err]
+  }
+
+  solutionComplianceEvidencePageGet(req, res)
+}
+
+async function downloadEvidenceGet (req, res) {
+  const context = {
+    ...await evidencePageContext(req)
+  }
+  req.body.errors = req.body.erros || []
+
+  const claimID = req.params.claim_id
+  const fileName = req.params.file_name
+
+  const selectedFile = _.find(context.files.items, ['name', fileName] )
+
+  downloadFile(claimID, selectedFile.url).then((fileResponse) => {
+    res.header('Content-Disposition', `attachment; filename="${fileName}"`);
+    fileResponse.body.pipe(res)
+  }).catch((err) => {
+    req.body.errors.push(err)
+    solutionComplianceEvidencePageGet(req, res)
+  })
+}
+
+async function downloadFile (claimID, url) {
+  return sharePointProvider.downloadStdEvidence(claimID, url)
+}
+
+async function fetchFiles (claimID) {
+  console.log('\n\nRequesting Evidence Listing:', claimID, '\n\n')
+  return sharePointProvider.getStdEvidenceFiles(claimID)
+}
+
+function findLatestFile(files) {
+  return _.maxBy(files, (f) => f.timeLastModified)
+}
+
+async function uploadFile (claimID, buffer, fileName) {
+  return sharePointProvider.uploadStdEvidence(claimID, buffer, fileName)
 }
 
 module.exports = router
