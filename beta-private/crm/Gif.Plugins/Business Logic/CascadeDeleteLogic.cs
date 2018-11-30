0906e6bbdc76743e4c9847b430021d0f2165b029
James Ackroyd <james.ackroyd@nhs.net>
2018-11-30 10:08:18 +0000

    change step to fire on delet of standard applicable

--- beta-private/crm/Gif.Plugins/Business Logic/CascadeDeleteLogic.cs
+++ beta-private/crm/Gif.Plugins/Business Logic/CascadeDeleteLogic.cs
@@ -22,75 +22,24 @@ namespace Gif.Plugins.Business_Logic
 
         public void OnSolutionDelete(Entity target)
         {
-            Trace($"Solution: {target.Id}");
-            Trace("Get technical contacts");
-            var technicalContacts = SolutionRepository.GetTechnicalContactsBySolution(target.Id);
-            var ccTechnicalcontacts = technicalContacts.ToList();
-            Trace($"technicalContacts count: {ccTechnicalcontacts.Count}");
-            foreach (var technicalContact in ccTechnicalcontacts)
+            Trace("Get evidences");
+            var evidences = SolutionRepository.GetEvidencesByStandardApplicable(target.Id);
+            var ccEvidences = evidences.ToList();
+            Trace($"evidences count: {ccEvidences.Count}");
+            foreach (var evidence in ccEvidences)
             {
-                Trace("Delete technical contact");
-                SolutionRepository.Delete(technicalContact);
-            }
-
-            Trace("Get capabilities implemented");
-            var capabilitiesImplemented = SolutionRepository.GetCapabilitiesImplementedBySolution(target.Id);
-            var ccCapabilitiesImplemented = capabilitiesImplemented.ToList();
-            Trace($"capabilitiesImplemented count: {ccCapabilitiesImplemented.Count}");
-            foreach (var capabilityImplemented in ccCapabilitiesImplemented)
-            {
-                Trace("Get evidences");
-                var evidences = SolutionRepository.GetEvidencesByCapabilityImplemented(capabilityImplemented.Id);
-                var ccEvidences = evidences.ToList();
-                Trace($"evidences count: {ccEvidences.Count}");
-                foreach (var evidence in ccEvidences)
+                Trace("Get reviews");
+                var reviews = SolutionRepository.GetReviewsByEvidence(evidence.Id);
+                var ccReviews = reviews.ToList();
+                Trace($"reviews count: {ccReviews.Count}");
+                foreach (var review in ccReviews)
                 {
-                    Trace("Get reviews");
-                    var reviews = SolutionRepository.GetReviewsByEvidence(evidence.Id);
-                    var ccReviews = reviews.ToList();
-                    Trace($"reviews count: {ccReviews.Count}");
-                    foreach (var review in ccReviews)
-                    {
-                        Trace("Delete review");
-                        SolutionRepository.Delete(review);
-                    }
-
-                    Trace("Delete evidence");
-                    SolutionRepository.Delete(evidence);
+                    Trace("Delete review");
+                    SolutionRepository.Delete(review);
                 }
 
-                Trace("Delete capability implemented");
-                SolutionRepository.Delete(capabilityImplemented);
-            }
-
-            Trace("Get standards applicable");
-            var standardsApplicable = SolutionRepository.GetStandardsApplicableBySolution(target.Id);
-            var ccStandardsApplicable = standardsApplicable.ToList();
-            Trace($"standardsApplicable count: {ccStandardsApplicable.Count}");
-            foreach (var standardApplicable in ccStandardsApplicable)
-            {
-                Trace("Get evidences");
-                var evidences = SolutionRepository.GetEvidencesByStandardApplicable(standardApplicable.Id);
-                var ccEvidences = evidences.ToList();
-                Trace($"evidences count: {ccEvidences.Count}");
-                foreach (var evidence in ccEvidences)
-                {
-                    Trace("Get reviews");
-                    var reviews = SolutionRepository.GetReviewsByEvidence(evidence.Id);
-                    var ccReviews = reviews.ToList();
-                    Trace($"reviews count: {ccReviews.Count}");
-                    foreach (var review in ccReviews)
-                    {
-                        Trace("Delete review");
-                        SolutionRepository.Delete(review);
-                    }
-
-                    Trace("Delete evidence");
-                    SolutionRepository.Delete(evidence);
-                }
-
-                Trace("Delete standard applicable");
-                SolutionRepository.Delete(standardApplicable);
+                Trace("Delete evidence");
+                SolutionRepository.Delete(evidence);
             }
         }
     }
---
+++ beta-private/crm/Gif.TestHarness/CrmHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Configuration;
+using System.Net;
+
+namespace Gif.TestHarness
+{
+    public static class CrmHelper
+    {
+        public static IOrganizationService GetService()
+        {
+            IOrganizationService service = null;
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var connectionString = ConfigurationManager.ConnectionStrings["Crm"].ConnectionString;
+            if (connectionString == null) throw new ArgumentNullException(nameof(connectionString));
+            var connection = new CrmServiceClient(connectionString);
+            var proxy = connection.OrganizationServiceProxy;
+
+            proxy.Timeout = new TimeSpan(0, 0, 2, 0);
+            proxy.ServiceConfiguration.CurrentServiceEndpoint.Behaviors.Add(new ProxyTypesBehavior());
+
+            return proxy;
+        }
+    }
+}
