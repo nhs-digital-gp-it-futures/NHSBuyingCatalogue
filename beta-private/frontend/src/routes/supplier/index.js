f1aef839f7e8043ec1782693f74b099de0cee7b6
Paul Mitchell (BJSS) <paul.mitchell@bjss.com>
2018-12-10 10:44:20 +0000

    🔩 #609 Automation test fixes for Onboarding Dashboard and data model
    This only changes the baseline tests so that they pass. It adds no new tests.
    
    - enforce order of solutions on dashboard to prevent occasional test failures
    - adapt to link label used in wireframe of dashboard
    - adapt to addition of Interoperability Standard to seed data

--- beta-private/frontend/src/routes/supplier/index.js
+++ beta-private/frontend/src/routes/supplier/index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const router = require('express').Router({ strict: true, mergeParams: true })
 const { dataProvider } = require('catalogue-data')
 
@@ -23,6 +24,9 @@ router.get('/', strictRouting, async (req, res) => {
       ...soln,
       url: `solutions/${soln.id}/#content`
     }))
+
+    context.solutions.onboarding = _.orderBy(context.solutions.onboarding, 'displayName')
+    context.solutions.live = _.orderBy(context.solutions.live, 'displayName')
   } catch (err) {
     context.errors.push(err)
   }
