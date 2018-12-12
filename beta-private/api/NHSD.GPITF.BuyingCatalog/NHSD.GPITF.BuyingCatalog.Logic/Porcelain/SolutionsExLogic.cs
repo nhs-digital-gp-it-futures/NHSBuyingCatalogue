28a080187c393c324c021af3b61120d1c5095d6f
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2018-12-12 08:37:58 +0000

    - make SolutionsEx.Update consistent with Solution.Update
    -- update ModifiedBy/On
    -- create SharePoint folders

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/Porcelain/SolutionsExLogic.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/Porcelain/SolutionsExLogic.cs
@@ -1,7 +1,10 @@
 ﻿using FluentValidation;
 using Microsoft.AspNetCore.Http;
+using NHSD.GPITF.BuyingCatalog.Interfaces;
 using NHSD.GPITF.BuyingCatalog.Interfaces.Porcelain;
+using NHSD.GPITF.BuyingCatalog.Models;
 using NHSD.GPITF.BuyingCatalog.Models.Porcelain;
+using System;
 using System.Linq;
 
 namespace NHSD.GPITF.BuyingCatalog.Logic.Porcelain
@@ -11,17 +14,23 @@ namespace NHSD.GPITF.BuyingCatalog.Logic.Porcelain
     private readonly ISolutionsExDatastore _datastore;
     private readonly ISolutionsExValidator _validator;
     private readonly ISolutionsExFilter _filter;
+    private readonly IContactsDatastore _contacts;
+    private readonly IEvidenceBlobStoreLogic _evidenceBlobStoreLogic;
 
     public SolutionsExLogic(
       ISolutionsExDatastore datastore,
       IHttpContextAccessor context,
       ISolutionsExValidator validator,
-      ISolutionsExFilter filter) :
+      ISolutionsExFilter filter,
+      IContactsDatastore contacts,
+      IEvidenceBlobStoreLogic evidenceBlobStoreLogic) :
       base(context)
     {
       _datastore = datastore;
       _validator = validator;
       _filter = filter;
+      _contacts = contacts;
+      _evidenceBlobStoreLogic = evidenceBlobStoreLogic;
     }
 
     public SolutionEx BySolution(string solutionId)
@@ -32,6 +41,16 @@ namespace NHSD.GPITF.BuyingCatalog.Logic.Porcelain
     public void Update(SolutionEx solnEx)
     {
       _validator.ValidateAndThrow(solnEx, ruleSet: nameof(ISolutionsExLogic.Update));
+
+      var email = Context.Email();
+      solnEx.Solution.ModifiedById = _contacts.ByEmail(email).Id;
+      solnEx.Solution.ModifiedOn = DateTime.UtcNow;
+
+      // create SharePoint folder structure
+      if (solnEx.Solution.Status == SolutionStatus.Registered)
+      {
+        _evidenceBlobStoreLogic.PrepareForSolution(solnEx.Solution.Id);
+      }
 
       _datastore.Update(solnEx);
     }
