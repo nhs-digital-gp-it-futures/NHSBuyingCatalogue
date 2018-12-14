6bf819ef961ce3c31f1cb9f6f0e148addfb0cce1
Adam Slack <adam.slack@bjss.com>
2018-12-14 09:08:44 +0000

    [Fake SharePoint Provider] Moved the Providers into a seperate folder

--- beta-private/frontend/lib/catalogue-sharepoint/index.js
+++ beta-private/frontend/lib/catalogue-sharepoint/index.js
@@ -1,10 +1,10 @@
 
-const FakeSharePointProvider = require('./FakeSharePointProvider')
-const RealSharePointProvider = require('./RealSharePointProvider')
+const FakeSharePointProvider = require('./providers/FakeSharePointProvider')
+const RealSharePointProvider = require('./providers/RealSharePointProvider')
 
 module.exports = {
   sharePointProvider: process.env.SHAREPOINT_PROVIDER_ENV === 'test'
     ? new FakeSharePointProvider()
     : new RealSharePointProvider(),
-  SharePointProvider: require('./SharePointProvider')
+  SharePointProvider: require('./providers/SharePointProvider')
 }
