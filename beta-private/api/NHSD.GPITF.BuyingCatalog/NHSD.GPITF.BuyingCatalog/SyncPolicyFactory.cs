08a68aa6f197f5333532dcc1379a97cc58307d7a
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-01-03 10:19:27 +0000

    - less retries so we fail sooner

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog/SyncPolicyFactory.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog/SyncPolicyFactory.cs
@@ -11,7 +11,7 @@ namespace NHSD.GPITF.BuyingCatalog
     public ISyncPolicy Build(ILogger logger)
     {
       return Policy.Handle<Exception>()
-        .WaitAndRetry(6, // We can also do this with WaitAndRetryForever... but chose WaitAndRetry this time.
+        .WaitAndRetry(3, // We can also do this with WaitAndRetryForever... but chose WaitAndRetry this time.
           attempt => TimeSpan.FromSeconds(0.1 * Math.Pow(2, attempt)), // Back off!  2, 4, 8, 16 etc times 1/4-second
             (exception, calculatedWaitDuration) =>  // Capture some info for logging!
             {
