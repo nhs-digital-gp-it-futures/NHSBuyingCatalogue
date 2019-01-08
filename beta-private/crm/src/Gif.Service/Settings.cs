f285e4507bd6227faaa10faf25dbb543a86449f1
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-01-08 09:47:10 +0000

    - refactored

--- beta-private/crm/src/Gif.Service/Settings.cs
+++ beta-private/crm/src/Gif.Service/Settings.cs
@@ -5,32 +5,10 @@ namespace Gif.Service
 {
   internal static class Settings
   {
-    public static string GIF_AUTHORITY_URI(IConfiguration config)
-    {
-      return
-        Environment.GetEnvironmentVariable("GIF_AUTHORITY_URI") ??
-        config["GIF:Authority_Uri"] ??
-        "http://localhost:5001";
-    }
-
-    public static string GIF_CRM_AUTHORITY(IConfiguration config)
-    {
-      return Environment.GetEnvironmentVariable("GIF_CRM_AUTHORITY") ?? config["CrmAuthority"];
-    }
-
-    public static string GIF_CRM_URL(IConfiguration config)
-    {
-      return Environment.GetEnvironmentVariable("GIF_CRM_URL") ?? config["CrmUrl"];
-    }
-
-    public static string GIF_AZURE_CLIENT_ID(IConfiguration config)
-    {
-      return Environment.GetEnvironmentVariable("GIF_AZURE_CLIENT_ID") ?? config["AzureClientId"];
-    }
-
-    public static string GIF_ENCRYPTED_CLIENT_SECRET(IConfiguration config)
-    {
-      return Environment.GetEnvironmentVariable("GIF_ENCRYPTED_CLIENT_SECRET") ?? config["EncryptedClientSecret"];
-    }
+    public static string GIF_AUTHORITY_URI(IConfiguration config) => Environment.GetEnvironmentVariable("GIF_AUTHORITY_URI") ?? config["GIF:Authority_Uri"] ?? "http://localhost:5001";
+    public static string GIF_CRM_AUTHORITY(IConfiguration config) => Environment.GetEnvironmentVariable("GIF_CRM_AUTHORITY") ?? config["CrmAuthority"];
+    public static string GIF_CRM_URL(IConfiguration config) => Environment.GetEnvironmentVariable("GIF_CRM_URL") ?? config["CrmUrl"];
+    public static string GIF_AZURE_CLIENT_ID(IConfiguration config) => Environment.GetEnvironmentVariable("GIF_AZURE_CLIENT_ID") ?? config["AzureClientId"];
+    public static string GIF_ENCRYPTED_CLIENT_SECRET(IConfiguration config) => Environment.GetEnvironmentVariable("GIF_ENCRYPTED_CLIENT_SECRET") ?? config["EncryptedClientSecret"];
   }
 }
