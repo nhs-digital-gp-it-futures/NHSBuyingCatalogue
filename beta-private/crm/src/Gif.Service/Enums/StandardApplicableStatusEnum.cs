3d11d7158b27c22b336a66300b82ad45be637af4
James Corry <james.corry@nhs.net>
2019-01-31 09:56:30 +0000

    - Updated the standard applicable status enum which appeared to have duplicate values

--- beta-private/crm/src/Gif.Service/Enums/StandardApplicableStatusEnum.cs
+++ beta-private/crm/src/Gif.Service/Enums/StandardApplicableStatusEnum.cs
@@ -8,13 +8,13 @@ namespace Gif.Service.Enums
         Approved = 948120004,
 
         [EnumMember(Value = "Approved First Of Type")]
-        ApprovedFirstOfType = 948120004,
+        ApprovedFirstOfType = 948120005,
 
         [EnumMember(Value = "ApprovedPartial")]
-        ApprovedPartial = 948120004,
+        ApprovedPartial = 948120006,
 
         [EnumMember(Value = "Draft")]
-        Draft = 948120004,
+        Draft = 948120001,
 
         [EnumMember(Value = "NotStarted")]
         NotStarted = 948120000,
