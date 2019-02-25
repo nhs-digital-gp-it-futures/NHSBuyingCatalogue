940cd8340ed66fcd990c5adfc4492e26ecff841c
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-02-25 12:09:57 +0000

    - set OriginalDate on Create

---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Tests/DummyReviewsLogicBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using NHSD.GPITF.BuyingCatalog.Interfaces;
+using NHSD.GPITF.BuyingCatalog.Logic;
+using NHSD.GPITF.BuyingCatalog.Models;
+using System.Collections.Generic;
+
+namespace NHSD.GPITF.BuyingCatalog.Tests
+{
+  public sealed class DummyReviewsLogicBase : ReviewsLogicBase<ReviewsBase>
+  {
+    public DummyReviewsLogicBase(
+      IReviewsDatastore<ReviewsBase> datastore,
+      IContactsDatastore contacts,
+      IReviewsValidator<ReviewsBase> validator,
+      IReviewsFilter<IEnumerable<ReviewsBase>> filter,
+      IHttpContextAccessor context) :
+      base(datastore, contacts, validator, filter, context)
+    {
+    }
+  }
+}
