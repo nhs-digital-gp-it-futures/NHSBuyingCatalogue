e862bb5c2ced3f25dfa8f7dc81a90203119f6dd0
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-03-07 11:27:14 +0000

    - formatting

--- beta-private/crm/src/Gif.Service/Crm/IRepository.cs
+++ beta-private/crm/src/Gif.Service/Crm/IRepository.cs
@@ -6,14 +6,14 @@ using System.Collections.Generic;
 
 namespace Gif.Service.Crm
 {
-    public interface IRepository
-    {
-        JToken RetrieveMultiple(string query, out int? count);
-        void Associate(Guid entityId1, string entName1, Guid entityId2, string entName2, string relationshipKey);
-        Guid CreateEntity(string entityName, string entityData, bool update = false);
-        void UpdateEntity(string entityName, Guid id, string entityData);
-        void CreateBatch(List<BatchData> batchData);
-        void Delete(string entityName, Guid id);
-    }
+  public interface IRepository
+  {
+    JToken RetrieveMultiple(string query, out int? count);
+    void Associate(Guid entityId1, string entName1, Guid entityId2, string entName2, string relationshipKey);
+    Guid CreateEntity(string entityName, string entityData, bool update = false);
+    void UpdateEntity(string entityName, Guid id, string entityData);
+    void CreateBatch(List<BatchData> batchData);
+    void Delete(string entityName, Guid id);
+  }
 }
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
