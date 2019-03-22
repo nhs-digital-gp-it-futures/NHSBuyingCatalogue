2539b271fa3b477deaaf14238484d287f0438a71
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-03-22 13:53:00 +0000

    - remove hardcoded values & get from config

--- beta-private/crm/src/Gif.Service/AuthConfig.cs
+++ beta-private/crm/src/Gif.Service/AuthConfig.cs
@@ -1,43 +1,41 @@
-﻿using System;
+﻿using IdentityServer4.Models;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using IdentityServer4.Models;
 
 namespace Gif.Service
 {
-    public class AuthConfig
+  public class AuthConfig
+  {
+    public static IEnumerable<Client> GetClients(IConfiguration config)
     {
-        public static IEnumerable<Client> GetClients()
+      return new List<Client>
+      {
+        new Client
         {
-            return new List<Client>
-            {
-                new Client
-                {
-                    ClientId = "a3c677e9-1867-4456-883a-4f6fdca9ba6e",
+          ClientId = Settings.CRM_CLIENTID(config),
 
-                    // no interactive user, use the clientid/secret for authentication
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+          // no interactive user, use the clientid/secret for authentication
+          AllowedGrantTypes = GrantTypes.ClientCredentials,
 
-                    // secret for authentication
-                    ClientSecrets =
-                    {
-                        new Secret("5Xe4mJi6Rcb4xL+3y/91XAqPwaUG3Dj9DVkuWE2wZxQ=".Sha256())
-                    },
+          // secret for authentication
+          ClientSecrets =
+          {
+            new Secret(Settings.CRM_CLIENTSECRET(config).Sha256())
+          },
 
-                    // scopes that client has access to
-                    AllowedScopes = { "GIFBuyingCatalogue" },
-                    AccessTokenLifetime = 7200 //2 hours
-                }
-            };
+          // scopes that client has access to
+          AllowedScopes = { "GIFBuyingCatalogue" },
+          AccessTokenLifetime = 7200 //2 hours
         }
-
-        public static IEnumerable<ApiResource> GetApiResources()
-        {
-            return new List<ApiResource>
-            {
-                new ApiResource("GIFBuyingCatalogue", "GIF Buying Catalogue")
-            };
-        }
+      };
     }
+
+    public static IEnumerable<ApiResource> GetApiResources()
+    {
+      return new List<ApiResource>
+      {
+        new ApiResource("GIFBuyingCatalogue", "GIF Buying Catalogue")
+      };
+    }
+  }
 }
