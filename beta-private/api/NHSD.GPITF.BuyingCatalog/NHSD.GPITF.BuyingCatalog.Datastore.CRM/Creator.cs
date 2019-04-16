23471dca44c6e331aa56adf568298f8f5061d652
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-04-16 15:23:09 +0100

    - integrate GifInt.IFrameworksDatastore directly into API

---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/Creator.cs
@@ -0,0 +1,19 @@
+﻿using NHSD.GPITF.BuyingCatalog.Models;
+using GifModels = Gif.Service.Models;
+
+namespace NHSD.GPITF.BuyingCatalog.Datastore.CRM
+{
+  internal static class Creator
+  {
+    internal static Frameworks FromCrm(GifModels.Framework crm)
+    {
+      return new Frameworks
+      {
+        Id = crm.Id.ToString(),
+        PreviousId = crm.PreviousId?.ToString(),
+        Name = crm.Name,
+        Description = crm.Description
+      };
+    }
+  }
+}
---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Datastore.CRM/CrmDatastoreBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using NHSD.GPITF.BuyingCatalog.Interfaces;
+using Polly;
+using System;
+
+namespace NHSD.GPITF.BuyingCatalog.Datastore.CRM
+{
+  public abstract class CrmDatastoreBase<T>
+  {
+    protected readonly ILogger<CrmDatastoreBase<T>> _logger;
+    private readonly ISyncPolicy _policy;
+
+    public CrmDatastoreBase(
+      ILogger<CrmDatastoreBase<T>> logger,
+      ISyncPolicyFactory policy)
+    {
+      _logger = logger;
+      _policy = policy.Build(_logger);
+    }
+
+    protected TOther GetInternal<TOther>(Func<TOther> get)
+    {
+      return _policy.Execute(get);
+    }
+  }
+}
