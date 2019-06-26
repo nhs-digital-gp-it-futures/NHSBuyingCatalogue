bbbcc3a62690d0fb384833b112f0a5f4a4ea864b
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-06-26 10:05:50 +0100

    - initial version of CRM-->datastore importer

---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/Gif.Datastore.Importer/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using NHSD.GPITF.BuyingCatalog.Datastore.Database;
+using System;
+
+namespace Gif.Datastore.Importer
+{
+  public sealed class Program
+  {
+    static void Main(string[] args)
+    {
+      new Program(args).Run();
+    }
+
+    private IConfigurationRoot Configuration { get; }
+
+    public Program(string[] args)
+    {
+      Configuration = new ConfigurationBuilder()
+        .AddJsonFile("hosting.json")
+        .AddUserSecrets<Program>()
+        .Build();
+
+      DumpSettings();
+      Console.WriteLine();
+    }
+
+    private void Run()
+    {
+      var dbConnFact = new DbConnectionFactory(Configuration);
+      using (var conn = dbConnFact.Get())
+      {
+        Console.WriteLine($"Importing from:  {Settings.GIF_CRM_URL(Configuration)}");
+        Console.WriteLine($"          into:  {conn.ConnectionString}");
+      }
+    }
+
+    private void DumpSettings()
+    {
+      Console.WriteLine("Settings:");
+      Console.WriteLine($"  CRM:");
+      Console.WriteLine($"    CRM_CLIENTID            : {Settings.CRM_CLIENTID(Configuration)}");
+      Console.WriteLine($"    CRM_CLIENTSECRET        : {Settings.CRM_CLIENTSECRET(Configuration)}");
+
+      Console.WriteLine($"  GIF:");
+      Console.WriteLine($"    GIF_CRM_URL                 : {Settings.GIF_CRM_URL(Configuration)}");
+      Console.WriteLine($"    GIF_AUTHORITY_URI           : {Settings.GIF_AUTHORITY_URI(Configuration)}");
+      Console.WriteLine($"    GIF_AZURE_CLIENT_ID         : {Settings.GIF_AZURE_CLIENT_ID(Configuration)}");
+      Console.WriteLine($"    GIF_ENCRYPTED_CLIENT_SECRET : {Settings.GIF_ENCRYPTED_CLIENT_SECRET(Configuration)}");
+
+      Console.WriteLine($"  DATASTORE:");
+      Console.WriteLine($"    DATASTORE_CONNECTION        : {Settings.DATASTORE_CONNECTION(Configuration)}");
+      Console.WriteLine($"    DATASTORE_CONNECTIONTYPE    : {Settings.DATASTORE_CONNECTIONTYPE(Configuration, Settings.DATASTORE_CONNECTION(Configuration))}");
+      Console.WriteLine($"    DATASTORE_CONNECTIONSTRING  : {Settings.DATASTORE_CONNECTIONSTRING(Configuration, Settings.DATASTORE_CONNECTION(Configuration))}");
+    }
+  }
+}
---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/Gif.Datastore.Importer/Settings.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Gif.Datastore.Importer
+{
+  public static class Settings
+  {
+    public static string CRM_CLIENTID(IConfiguration config) => Environment.GetEnvironmentVariable("CRM_CLIENTID") ?? config["CRM:ClientId"];
+    public static string CRM_CLIENTSECRET(IConfiguration config) => Environment.GetEnvironmentVariable("CRM_CLIENTSECRET") ?? config["CRM:ClientSecret"];
+
+    public static string GIF_CRM_URL(IConfiguration config) => Environment.GetEnvironmentVariable("GIF_CRM_URL") ?? config["CrmUrl"];
+    public static string GIF_AUTHORITY_URI(IConfiguration config) => Environment.GetEnvironmentVariable("GIF_AUTHORITY_URI") ?? config["GIF:Authority_Uri"] ?? "http://localhost:5001";
+    public static string GIF_AZURE_CLIENT_ID(IConfiguration config) => Environment.GetEnvironmentVariable("GIF_AZURE_CLIENT_ID") ?? config["AzureClientId"];
+    public static string GIF_ENCRYPTED_CLIENT_SECRET(IConfiguration config) => Environment.GetEnvironmentVariable("GIF_ENCRYPTED_CLIENT_SECRET") ?? config["EncryptedClientSecret"];
+
+    public static string DATASTORE_CONNECTION(IConfiguration config) => Environment.GetEnvironmentVariable("DATASTORE_CONNECTION") ?? config["RepositoryDatabase:Connection"];
+    public static string DATASTORE_CONNECTIONTYPE(IConfiguration config, string connection) => Environment.GetEnvironmentVariable("DATASTORE_CONNECTIONTYPE") ?? config[$"RepositoryDatabase:{connection}:Type"];
+    public static string DATASTORE_CONNECTIONSTRING(IConfiguration config, string connection) => (Environment.GetEnvironmentVariable("DATASTORE_CONNECTIONSTRING") ?? config[$"RepositoryDatabase:{connection}:ConnectionString"]);
+  }
+}
