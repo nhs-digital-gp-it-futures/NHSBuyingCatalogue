570da1990c3655654353aaaba0068c099f04832a
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-06-27 15:03:12 +0100

    - formatting

--- beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/Notifiers/SolutionChangeNotifier.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Logic/Notifiers/SolutionChangeNotifier.cs
@@ -4,7 +4,7 @@ using NHSD.GPITF.BuyingCatalog.Models;
 
 namespace NHSD.GPITF.BuyingCatalog.Logic.Notifiers
 {
-  public sealed class SolutionChangeNotifier :ChangeNotifierBase<Solutions>, ISolutionsChangeNotifier
+  public sealed class SolutionChangeNotifier : ChangeNotifierBase<Solutions>, ISolutionsChangeNotifier
   {
     public SolutionChangeNotifier(IConfiguration config) :
       base(config)
