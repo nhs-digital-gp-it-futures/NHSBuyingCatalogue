d5a97c5952dee82480eb318fa1e5b228742512a8
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-07-03 11:13:25 +0100

    - added assy for receiving notifications
    -- still have to work out best way to host/run receivers esp when scaling

---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Receiver.AMQP/ChangeReceiverBase.cs
@@ -0,0 +1,46 @@
+﻿using Amqp;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Text;
+
+namespace NHSD.GPITF.BuyingCatalog.Receiver.AMQP
+{
+  public abstract class ChangeReceiverBase<T>
+  {
+    private readonly bool _isAzure;
+    private readonly string _topicPrefix;
+
+    protected ChangeReceiverBase(IConfiguration config)
+    {
+      _topicPrefix = Settings.AMQP_TOPIC_PREFIX(config);
+      _isAzure = Settings.USE_AZURE_SERVICE_BUS(config);
+
+      var protocol = Settings.AMQP_PROTOCOL(config);
+      var policyName = Settings.AMQP_POLICY_NAME(config);
+      var policyKey = Settings.AMQP_POLICY_KEY(config);
+      var namespaceUrl = Settings.AMQP_NAMESPACE_URL(config);
+      var connStr = $"{protocol}://{policyName}:{policyKey}@{namespaceUrl}/";
+      var address = new Address(connStr);
+      var connection = new Connection(address);
+      var session = new Session(connection);
+      var topicAddress = GetTopicAddress();
+      var receiver = new ReceiverLink(session, GetType().Name, topicAddress);
+
+      receiver.Start(100, (rec, msg) =>
+      {
+        ProcessMessage(rec, msg);
+
+        rec.Accept(msg);
+      });
+    }
+
+    protected abstract void ProcessMessage(IReceiverLink rec, Message msg);
+
+    private string GetTopicAddress()
+    {
+      return _isAzure ?
+        $"{_topicPrefix}{typeof(T).Name}/Subscriptions/{typeof(T).Name}" :
+        $"{_topicPrefix}{typeof(T).Name}";
+    }
+  }
+}
---
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/NHSD.GPITF.BuyingCatalog.Receiver.AMQP/SolutionChangeReceiver.cs
@@ -0,0 +1,36 @@
+﻿using Amqp;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using NHSD.GPITF.BuyingCatalog.Interfaces;
+using NHSD.GPITF.BuyingCatalog.Interfaces.Interfaces;
+using NHSD.GPITF.BuyingCatalog.Models;
+using System.Text;
+
+namespace NHSD.GPITF.BuyingCatalog.Receiver.AMQP
+{
+  public sealed class SolutionChangeReceiver : ChangeReceiverBase<Solutions>
+  {
+    private readonly IEvidenceBlobStoreLogic _evidenceBlobStoreLogic;
+
+    public SolutionChangeReceiver(
+      IConfiguration config,
+      IEvidenceBlobStoreLogic evidenceBlobStoreLogic) :
+      base(config)
+    {
+      _evidenceBlobStoreLogic = evidenceBlobStoreLogic;
+    }
+
+    protected override void ProcessMessage(IReceiverLink rec, Message msg)
+    {
+      var json = Encoding.UTF8.GetString((byte[])msg.Body);
+      var record = JsonConvert.DeserializeObject<ChangeRecord<Solutions>>(json);
+      var solution = record.NewVersion;
+
+      // create SharePoint folder structure
+      if (solution.Status == SolutionStatus.Registered)
+      {
+        _evidenceBlobStoreLogic.PrepareForSolution(solution.Id);
+      }
+    }
+  }
+}
