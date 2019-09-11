fde0c7be081bbe6575c04e76fff908103cc50124
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-09-11 07:13:20 +0100

    - code cleanup

--- beta-private/api/NHSD.GPITF.BuyingCatalog/crm/Gif.Datastore.CRM/Services/Equality/ClaimedCapabilityIdComparator.cs
+++ beta-private/api/NHSD.GPITF.BuyingCatalog/crm/Gif.Datastore.CRM/Services/Equality/ClaimedCapabilityIdComparator.cs
@@ -1,9 +1,8 @@
-﻿using Gif.Service.Contracts;
-using Gif.Service.Models;
+﻿using Gif.Service.Models;
 
 namespace Gif.Service.Services.Equality
 {
-    public class ClaimedCapabilityIdComparator :  EqualityIdComparatorBase<CapabilityImplemented>
+  public class ClaimedCapabilityIdComparator :  EqualityIdComparatorBase<CapabilityImplemented>
     {
     }
 }
